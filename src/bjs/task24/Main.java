5edee042e8c36885eb72991ecb029cc98800d852
Yuriy Mykytenko <tairelus@gmail.com>
2015-12-20 19:59:27 +0200

    Task24: Implemented

---
+++ src/bjs/task24/Main.java
@@ -0,0 +1,50 @@
+package bjs.task24;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MessageLoop messageLoop = new MessageLoop(3, 4000);
+        System.out.println("Start " + MessageLoop.class.getSimpleName() + " thread");
+
+        messageLoop.start("MessageLoop"); //Thread.getName() returns default thread name, not user defined. Why?
+        System.out.println("Waiting until " + MessageLoop.class.getSimpleName() + " will be finished\n");
+
+        while (messageLoop.getThread().isAlive()) {
+            try{
+                System.out.println(Thread.currentThread().getName() + ": Waiting...");
+                //Sleep main thread
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e) {
+                System.out.println(Thread.currentThread().getName() + ": thread interrupted. " + e.getMessage());
+            }
+        }
+
+        messageLoop.join();
+        System.out.println("\n" + Thread.currentThread().getName() + ": Finished!");
+    }
+}
+
+/*
+Start MessageLoop thread
+Waiting until MessageLoop will be finished
+
+main: Waiting...
+	Thread-0: Array message #1
+main: Waiting...
+main: Waiting...
+main: Waiting...
+main: Waiting...
+	Thread-0: Array message #2
+main: Waiting...
+main: Waiting...
+main: Waiting...
+	Thread-0: Array message #3
+main: Waiting...
+main: Waiting...
+main: Waiting...
+main: Waiting...
+main: Waiting...
+
+main: Finished!
+ */
---
+++ src/bjs/task24/MessageLoop.java
@@ -0,0 +1,84 @@
+package bjs.task24;
+
+/**
+ * Created by YM on 20.12.2015.
+ */
+public class MessageLoop implements Runnable{
+    /**Array of the messages*/
+    private String[] messagesArray;
+    /**Interval between messages in ms*/
+    private int messagesInterval;
+
+    /**Current thread*/
+    private Thread thread;
+
+    /**
+     * Constructs MessageLoop with several messages
+     * @param messagesNum Number of the messages
+     * @param messagesInterval Interval between messages in ms
+     */
+    public MessageLoop(int messagesNum, int messagesInterval) {
+        this.messagesInterval = messagesInterval;
+        thread = new Thread(this);
+        messagesArray = new String[messagesNum];
+
+
+        for (int i = 0; i < messagesArray.length; i++) {
+            StringBuilder message = new StringBuilder(messagesNum);
+
+            message.append("\t");
+            message.append(thread.getName());
+            message.append(": ");
+            message.append("Array message #");
+            message.append(i + 1);
+
+            messagesArray[i] = message.toString();
+        }
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < messagesArray.length; i++) {
+            System.out.println(messagesArray[i]);
+
+            try{
+                //Sleep thread for messagesInterval milliseconds
+                Thread.sleep(this.messagesInterval);
+            }
+            catch(InterruptedException e) {
+                System.out.println(thread.getName() + ": thread interrupted. " + e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * Gets current thread
+     * @return
+     */
+    public Thread getThread() {
+        return thread;
+    }
+
+    /**
+     * Thread name
+     * @param threadName Name of the current thread
+     */
+    public void start(String threadName) {
+        if (!threadName.isEmpty())
+            thread.setName(threadName);
+
+        thread.start();
+    }
+
+    /**
+     * Wrapper for Thread.join
+     */
+    public void join() {
+        try{
+            thread.join();
+        }
+        catch(InterruptedException e) {
+            System.out.println(thread.getName() + ": thread interrupted. " + e.getMessage());
+        }
+    }
+}
